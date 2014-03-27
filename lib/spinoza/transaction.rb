7e3ac41226a7895059591616cd7f30c4ed5d592e
Joel VanderWerf <vjoel@users.sourceforge.net>
2014-03-27 13:40:33 -0700

    simple transactions

--- lib/spinoza/transaction.rb
+++ lib/spinoza/transaction.rb
@@ -1,7 +1,26 @@
 class Transaction
 
-# simple txn is just a list of operations on specific table and row id pairs.
+  # simple txn is just a list of operations on specific table and row id pairs.
+  #
+  # complex txn is SQL
+  #
+  def initialize
+    @ops = []
+  end
+  
+  def insert table, row
+    @ops << [:insert, row]
+  end
 
-# complex txn is SQL
+  def update table, row
+    @ops << [:update, row]
+  end
 
+  def delete table, row_id
+    @ops << [:delete, row_id]
+  end
+
+  def sql
+    # mock the sequel api, recording methods to be applied later
+  end
 end
