16d285a5868fefd3d1e9bd52e7276ba8251cc275
Joel VanderWerf <vjoel@users.sourceforge.net>
2014-06-01 11:04:12 -0700

    Test Readcaster#serve_reads.

--- test/test-readcaster.rb
+++ test/test-readcaster.rb
@@ -30,27 +30,46 @@ class TestReadcaster < Minitest::Test
     @node.link @na, latency: 1.0
     @node.link @nb, latency: 1.0
     
-    # normally this is part of a Calvin::Node, but here we have a System::Node
-    @readcaster = Calvin::Readcaster.new(node: @node)
-  end
-  
-  def test_readcaster
     store = @node.store
     
     op_ia = InsertOperation.new(table: :as, row: {id: 2, name: "a2"})
     op_ib = InsertOperation.new(table: :bs, row: {id: 2, name: "b2"})
-    rslt = store.execute op_ia, op_ib
-    
-  
+    store.execute op_ia, op_ib
+
+    # normally this is part of a Calvin::Node, but here we have a System::Node
+    @readcaster = Calvin::Readcaster.new(node: @node)
+
     @txn = Transaction.new do
       at(:as).insert id: 1, name: "a1"
       at(:as, id: 2).read
       at(:bs, id: 2).read
     end
-
+  end
+  
+  def test_readcaster
     local_read_results = @readcaster.execute_local_reads @txn
     assert_equal 2, local_read_results.size
     assert_equal "a2", local_read_results[0].val[:name]
     assert_equal "b2", local_read_results[1].val[:name]
+  end
+  
+  def test_serve_reads
+    results = []
+    class << @readcaster; self; end.send :define_method,
+       :send_read do |link, **opts|
+      results << [link, opts]
+    end
+    
+    local_read_results = @readcaster.execute_local_reads @txn
+    @readcaster.serve_reads @txn, local_read_results
+
+    assert_equal 1, results.size
+    link, opts = results.first
+
+    assert_equal @node.links[@na], link
+    table, read_results = opts.values_at(:table, :read_results)
+    assert_equal :bs, table
+    assert_equal 1, read_results.size
+    assert_equal "b2", read_results[0].val[:name]
   end
 end
