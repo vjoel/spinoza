4f54fee0922e7c0a56464386de9439d2e3d2b13b
Joel VanderWerf <vjoel@users.sourceforge.net>
2014-06-04 13:07:50 -0700

    Fix tests.

--- test/test-transaction.rb
+++ test/test-transaction.rb
@@ -10,43 +10,72 @@ class TestTransaction < Minitest::Test
       Table[:foos, id: "integer", name: "string", len: "float"]
     ]
 
-    @txn = Transaction.new do
+    @txn1 = transaction do
       at(:foos).insert id: 1, name: "a", len: 1.2
       at(:foos).insert id: 2, name: "b", len: 3.4
+    end
+
+    @txn2 = transaction do
+      at(:foos, id: 1).read
       at(:foos, id: 2).read
-      at(:foos).insert id: 3, name: "c", len: 5.6
+      at(:foos, id: 3).read
+      at(:foos, id: 1).update len: 1.23
       at(:foos, id: 2).delete
+      at(:foos).insert id: 3, name: "c", len: 5.6
+     end
+
+    @txn3 = transaction do
+      at(:foos, id: 1).read
       at(:foos, id: 2).read
-      at(:foos, id: 3).update len: 7.8
       at(:foos, id: 3).read
     end
   end
 
   def test_txn
-    rslt = @node.store.execute(*@txn.ops)
+    rslt = @node.store.execute(*@txn1.ops)
+    assert_equal(0, rslt.size)
+
+    rslt = @node.store.execute(*@txn2.ops)
     assert_equal(3, rslt.size)
 
-    assert_equal({id: 2, name: "b", len: 3.4}, rslt[0].val)
-    refute rslt[1].val
-    assert_equal({id: 3, name: "c", len: 7.8}, rslt[2].val)
+    assert_equal({id: 1, name: "a", len: 1.2}, rslt[0].val)
+    assert_equal({id: 2, name: "b", len: 3.4}, rslt[1].val)
+    assert_equal(nil, rslt[2].val)
+
+    rslt = @node.store.execute(*@txn3.ops)
+    assert_equal(3, rslt.size)
+
+    assert_equal({id: 1, name: "a", len: 1.23}, rslt[0].val)
+    assert_equal(nil, rslt[1].val)
+    assert_equal({id: 3, name: "c", len: 5.6}, rslt[2].val)
   end
 
   def test_read_and_write_sets
-    assert_equal({foos: Set[{id: 2}, {id: 3}]}, @txn.read_set)
-    assert_equal({foos: Set[Transaction::INSERT_KEY, {id: 2}, {id: 3}]},
-      @txn.write_set)
-    assert_equal(Set[:foos], @txn.write_tables)
-    assert_equal(Set[:foos], @txn.read_tables)
+    assert_equal({}, @txn1.read_set)
+    assert_equal({foos: Set[Transaction::INSERT_KEY]}, @txn1.write_set)
+    assert_equal(Set[], @txn1.read_tables)
+    assert_equal(Set[:foos], @txn1.write_tables)
+
+    assert_equal({foos: Set[{id: 1}, {id: 2}, {id: 3}]}, @txn2.read_set)
+    assert_equal({foos: Set[Transaction::INSERT_KEY, {id: 1}, {id: 2}]},
+      @txn2.write_set)
+    assert_equal(Set[:foos], @txn2.read_tables)
+    assert_equal(Set[:foos], @txn2.write_tables)
+
+    assert_equal({foos: Set[{id: 1}, {id: 2}, {id: 3}]}, @txn3.read_set)
+    assert_equal({}, @txn3.write_set)
+    assert_equal(Set[:foos], @txn3.read_tables)
+    assert_equal(Set[], @txn3.write_tables)
   end
   
   def test_all_read_ops
-    assert_equal(3, @txn.all_read_ops.size)
-    assert_equal([{id: 2}, {id: 2}, {id: 3}],
-      @txn.all_read_ops.map {|op| op.key})
+    assert_equal(3, @txn2.all_read_ops.size)
+    assert_equal([{id: 1}, {id: 2}, {id: 3}],
+      @txn2.all_read_ops.map {|op| op.key})
   end
   
   def test_all_write_ops
-    assert_equal(5, @txn.all_write_ops.size)
+    assert_equal(3, @txn2.all_write_ops.size)
   end
 
   def test_active
@@ -55,7 +84,7 @@ class TestTransaction < Minitest::Test
       Table[:bars, id: "integer", name: "string", len: "float"]
     ]
     
-    txn = Transaction.new do
+    txn = transaction do
       at(:bars).insert id: 1, name: "a", len: 1.2
     end
     
